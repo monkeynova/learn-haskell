8d4102578c60cef3268f8d14f2f6681c2862ee7c
Keith Peters <keith@monkeynova.com>
2014-01-15 15:51:48 -0800

    now implement TAP with a slightly better condoned RWS monad...

--- tap/TAP.hs
+++ tap/TAP.hs
@@ -1,37 +1,57 @@
-module TAP (TAP.pass,TAP.fail,is,done_testing) where
+module TAP (TAP.pass,TAP.fail,is,done_testing,subtest) where
 
-import Data.IORef;
-import System.IO.Unsafe;
+import Control.Monad.RWS
 
-data TestState = TestState { testNum::Int }
+data TestState = TestState { curTestNum :: Int }
 
-{-# NOINLINE funcs #-}
-funcs :: IORef (TestState)
-funcs = unsafePerformIO $ newIORef (TestState 0)
+subtest f = runRWS f () (TestState 0)
 
-incTestNum :: IO ()
-incTestNum = atomicModifyIORef funcs (\s -> (s{testNum = (testNum s) + 1},()))
+curTest :: RWS () [String] TestState Int
+curTest = do
+          state <- get
+          return (curTestNum state)
+
+nextTest :: RWS () [String] TestState ()
+nextTest = do
+           state <- get
+           put state{ curTestNum = (curTestNum state) + 1 }
+           return ()
 
 showMsg msg = case msg of Nothing -> ""; Just str -> " " ++ str
 
-pass :: Maybe String -> IO ()
-pass msg = do
-           incTestNum
-           curState <- readIORef funcs
-           putStrLn $ "ok " ++ (show (testNum curState)) ++ (showMsg msg)
+pass :: Maybe String -> RWS () [String] TestState Bool
+pass msg = do 
+           nextTest
+           testNum <- curTest
+           tell [ "ok " ++ (show testNum) ++ showMsg msg ]
+           return True
 
-fail :: Maybe String -> IO ()
-fail msg = do
-           incTestNum
-           curState <- readIORef funcs
-           putStrLn $ "not ok " ++ (show (testNum curState)) ++ (showMsg msg)
-
-is :: Eq a => a -> a -> Maybe String -> IO ()
+fail :: Maybe String -> RWS () [String] TestState Bool
+fail msg = do 
+           nextTest
+           testNum <- curTest
+           tell [ "not ok " ++ (show testNum) ++ showMsg msg ]
+           return False
+       
+is :: Eq a => a -> a -> Maybe String -> RWS () [String] TestState Bool
 is got expect msg = do
                     if got == expect
-                    then TAP.pass msg
-                    else TAP.fail msg
+                    then
+                        TAP.pass msg
+                    else    
+                        TAP.fail msg
 
+isnt :: Eq a => a -> a -> Maybe String -> RWS () [String] TestState Bool
+isnt got expect msg = do
+                      if not( got == expect )
+                      then
+                          TAP.pass msg
+                      else    
+                          TAP.fail msg
+
+done_testing :: RWS () [String] TestState Bool
 done_testing = do
-               curState <- readIORef funcs
-               putStrLn $ "1.." ++ (show (testNum curState))
+               testNum <- curTest
+               tell [ "1.." ++ (show testNum) ]
+               return True
+
--- tap/tap.t.hs
+++ tap/tap.t.hs
@@ -1,9 +1,15 @@
 module Main where
 
+import Control.Monad.RWS
 import TAP
 
 main = do
-       pass $ Just "Foo"
-       pass Nothing
-       is 1 1 Nothing
-       done_testing
+       let (isOK,s,output) = subtest ( do
+                                          TAP.pass $ Just "Bar"
+                                          TAP.pass Nothing
+                                          TAP.fail $ Just "test failure"
+                                          is 1 1 Nothing
+                                          is 1 2 Nothing
+                                          done_testing
+                                        )
+       mapM_ putStrLn output
