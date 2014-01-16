007e845ce2e3f4bf34f4414d8c95de5309995c0f
Keith Peters <keith@monkeynova.com>
2014-01-16 13:10:36 -0800

    TAP.plan and TAP.or. hopefully I can find a way to improve the TAP.or syntax

--- tap/TAP.hs
+++ tap/TAP.hs
@@ -1,19 +1,42 @@
-module TAP (TAP.pass,TAP.fail,is,isnt,note,diag,done_testing,subtest,run_tests) where
+module TAP (TAP.pass,TAP.fail,ok,cmp_ok,is,isnt,note,diag,plan,PlanType(NoPlan,Tests),done_testing,subtest,run_tests) where
 
 import Control.Monad.RWS
+import System.IO
 
-data TestState = TestState { curTestNum :: Int, isDone :: Bool }
+data PlanType = NoPlan | Tests Int
 
-run_tests :: RWS () [String] TestState Bool -> IO ()
+type TestRead = ()
+type TestWrite = [(Handle,String)]
+data TestState = TestState { curTestNum :: Int, isDone :: Bool, indent :: String, testPlan :: Maybe PlanType }
+
+type TestReturn = RWS TestRead TestWrite TestState
+
+or :: TestReturn Bool -> TestReturn Bool -> TestReturn Bool
+or definitely maybe = do
+                      isOK <- definitely
+                      if isOK
+                      then
+                          return isOK
+                      else
+                          maybe
+
+run_tests :: TestReturn Bool -> IO ()
 run_tests f = do
-              let (isOK,s,output) = subtest f
-              mapM_ putStrLn output
+              let (isOK,s,output) = runRWS (do f; done_testing) () (TestState 0 False "" Nothing)
+              mapM_ (\(h,s) -> hPutStrLn h s) output
 
+plan :: PlanType -> TestReturn ()
+plan userPlan = do
+                modify (\s -> s{testPlan = Just userPlan})
+                case userPlan of
+                     Tests count -> do tell [ (stdout, "1.." ++ (show count)) ]
+                     NoPlan -> do return ()
+                
 
-subtest :: RWS () [String] TestState Bool -> (Bool, TestState, [String])
-subtest f = runRWS (do f; done_testing) () (TestState 0 False)
+subtest :: Maybe String -> TestReturn Bool -> TestReturn Bool
+subtest subtest f = return True
 
-nextTest :: RWS () [String] TestState ()
+nextTest :: TestReturn ()
 nextTest = do
            state <- get
            put state{ curTestNum = (curTestNum state) + 1 }
@@ -21,43 +44,60 @@ nextTest = do
 
 showMsg msg = case msg of Nothing -> ""; Just str -> " - " ++ str
 
-pass :: Maybe String -> RWS () [String] TestState Bool
+pass :: Maybe String -> TestReturn Bool
 pass msg = do 
            nextTest
            state <- get
-           tell [ "ok " ++ (show $ curTestNum state) ++ showMsg msg ]
+           tell [ (stdout, (indent state) ++ "ok " ++ (show $ curTestNum state) ++ showMsg msg) ]
            return True
 
-fail :: Maybe String -> RWS () [String] TestState Bool
+fail :: Maybe String -> TestReturn Bool
 fail msg = do 
            nextTest
            state <- get
-           tell [ "not ok " ++ (show $ curTestNum state) ++ showMsg msg ]
+           tell [ (stdout, (indent state) ++ "not ok " ++ (show $ curTestNum state) ++ showMsg msg) ]
            return False
        
-note :: String -> RWS () [String] TestState ()
+note :: String -> TestReturn ()
 note msg = do
-           tell [ "# " ++ msg ]
+           state <- get
+           tell [ (stdout, (indent state) ++ "# " ++ msg) ]
            return ()
 
-diag :: String -> RWS () [String] TestState ()
+diag :: String -> TestReturn ()
 diag msg = do
-           tell [ "# " ++ msg ]
+           state <- get
+           tell [ (stderr, (indent state) ++ "# " ++ msg) ]
            return ()
 
-is :: (Eq a,Show a) => a -> a -> Maybe String -> RWS () [String] TestState Bool
-is got expect msg = do
-                    if got == expect
-                    then
-                        TAP.pass msg
-                    else    
-                        do
-                        TAP.fail msg
-                        diag $ "         got = '" ++ (show got) ++ "'"
-                        diag $ "    expected = '" ++ (show expect) ++ "'"
+ok :: Bool -> Maybe String -> TestReturn Bool
+ok test msg = do
+              if test
+              then
+                  TAP.pass msg
+              else    
+                  TAP.fail msg
+
+cmp_show :: Ordering -> String
+cmp_show cmp = case cmp of
+               GT -> ">"
+               LT -> "<"
+               EQ -> "=="
+
+cmp_ok :: (Ord a,Show a) => a -> Ordering -> a -> Maybe String -> TestReturn Bool
+cmp_ok got cmp expect msg = ok (got `compare` expect == cmp) msg `TAP.or` do
+                        diag $ "     '" ++ (show got) ++ "'"
+                        diag $ "         " ++ (cmp_show cmp)
+                        diag $ "     '" ++ (show expect) ++ "'"
                         return False
 
-isnt :: (Eq a,Show a) => a -> a -> Maybe String -> RWS () [String] TestState Bool
+is :: (Eq a,Show a) => a -> a -> Maybe String -> TestReturn Bool
+is got expect msg = ok (got == expect) msg `TAP.or` do
+                        diag $ "          got = '" ++ (show got) ++ "'"
+                        diag $ "     expected = '" ++ (show expect) ++ "'"
+                        return False
+
+isnt :: (Eq a,Show a) => a -> a -> Maybe String -> TestReturn Bool
 isnt got expect msg = do
                       if not( got == expect )
                       then
@@ -69,7 +109,7 @@ isnt got expect msg = do
                         diag $ "    expected = anything else"
                         return False
 
-done_testing :: RWS () [String] TestState Bool
+done_testing :: TestReturn Bool
 done_testing = do
                state <- get
                if isDone state
@@ -79,6 +119,6 @@ done_testing = do
                else
                    do
                    modify (\s -> s{isDone = True})
-                   tell [ "1.." ++ (show $ curTestNum state) ]
+                   tell [ (stdout, (indent state) ++ "1.." ++ (show $ curTestNum state) ) ]
                    return True
 
--- tap/tap.t.hs
+++ tap/tap.t.hs
@@ -3,10 +3,22 @@ module Main where
 import TAP
 
 mainTests = do
+            plan $ Tests 1
             TAP.pass $ Just "Bar"
             TAP.pass Nothing
             TAP.fail $ Just "test failure"
-            is 1 1 Nothing
-            is 1 2 Nothing
+            is 1 1 $ Just "is true"
+            is 1 2 $ Just "is false"
+            cmp_ok 1 LT 2 $ Just "cmp_ok true"
+            cmp_ok 1 GT 2 $ Just "cmp_ok false"
+            subtest (Just "subtest-pass") ( do
+                    plan NoPlan
+                    TAP.pass $ Just "subtest sub-pass"
+               )
+            subtest (Just "subtest-fail") ( do
+                    plan $ Tests 3
+                    TAP.pass $ Just "subtest sub-pass"
+                    TAP.fail $ Just "subtest sub-fail"
+               )
 
 main = run_tests mainTests 
